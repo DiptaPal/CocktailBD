87c4a1d55bb46023e7149e1ffad795a0b8f7f300
DiptaPal <diptapal1998@gmail.com>
2022-09-01 14:41:49 +0600

    cocktailbd with some updated

--- js/app.js
+++ js/app.js
@@ -1,64 +1,79 @@
-const loadCocktail = (key) =>{
+const loadCocktail = (key, dataLimit) =>{
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${key}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayCocktail(data.drinks))
+    .then(data => displayCocktail(data.drinks, dataLimit))
     .catch(error => console.log(error))
 }
-const displayCocktail = (datas) =>{
+const displayCocktail = (datas , dataLimit) =>{
+    const searchResultNumber = document.getElementById('search-result-number');
+    const searchInfo = document.getElementById('search-info');
+    const errorInfo = document.getElementById('error-info');
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
-    datas.forEach(data => {
-        const card = document.createElement('div');
-        card.classList.add('card', 'bg-orange-500', 'shadow-xl');
-        if(data.strDrink.includes("Margarita","margarita") ){
-            card.innerHTML = `
-                <figure>
-                    <img src=${data.strDrinkThumb} alt="" class="w-full"/>
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title normal-case text-white">
-                        ${data.strDrink}
-                        <div class="badge bg-white text-orange-500 border-0">NEW</div>
-                    </h2>
-                    <p class="text-white text-justify">${data.strInstructions.slice(0,50)}</p>
-                    <div class="card-actions justify-start">
-                        <label onclick="loadDetail(${data.idDrink})" for="my-modal" class="btn bg-white text-orange-500 border-0 modal-button">View Details</label> 
-                    </div>
-                </div>
-            `
-        }
-        else{
-            card.innerHTML = `
-                <figure>
-                    <img src=${data.strDrinkThumb} alt="Shoes" class="w-full"/>
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title normal-case text-white">
-                        ${data.strDrink}
-                    </h2>
-                    <p class="text-white text-justify">${data.strInstructions.slice(0,50)}</p>
-                    <div class="card-actions justify-start">
-                        <button onclick="loadDetail(${data.idDrink})" for="my-modal" class="btn bg-white text-orange-500 border-0 modal-button">View Details</button> 
-                    </div>
-                </div>
-            `
-        }
-        cardContainer.appendChild(card);
+    if(datas === null){
+        errorInfo.classList.remove('hidden');
+        searchInfo.classList.add('hidden');
         sprint(false);
-    })  
-}
-
-document.getElementById('serach-filed').addEventListener('keyup', function (event){
-    sprint(true);
-    const serachField = document.getElementById('serach-filed');
-    const value = serachField.value
-    const commonvalue = value.toLowerCase();
-    if(event.key === "Enter"){
-        loadCocktail(commonvalue);
     }
     else{
-        loadCocktail(commonvalue);
+        console.log(dataLimit);
+        errorInfo.classList.add('hidden');
+        if(dataLimit && datas.length > 12){
+            searchResultNumber.innerText = datas.length;
+            searchInfo.classList.remove('hidden');
+            datas = datas.slice(0,12);
+        }
+        else{
+            searchInfo.classList.add('hidden');
+        }
+        datas.forEach(data => {
+            const card = document.createElement('div');
+            card.classList.add('card', 'bg-orange-500', 'shadow-xl');
+            if(data.strDrink.includes("Margarita") ){
+                card.innerHTML = `
+                    <figure>
+                        <img src=${data.strDrinkThumb} alt="" class="w-full"/>
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title normal-case text-white">
+                            ${data.strDrink}
+                            <div class="badge bg-white text-orange-500 border-0">NEW</div>
+                        </h2>
+                        <p class="text-white text-justify">${data.strInstructions.slice(0,50)}</p>
+                        <div class="card-actions justify-start">
+                            <label onclick="loadDetail(${data.idDrink})" for="my-modal" class="btn bg-white text-orange-500 border-0 modal-button">View Details</label> 
+                        </div>
+                    </div>
+                `
+            }
+            else{
+                card.innerHTML = `
+                    <figure>
+                        <img src=${data.strDrinkThumb} alt="Shoes" class="w-full"/>
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title normal-case text-white">
+                            ${data.strDrink}
+                        </h2>
+                        <p class="text-white text-justify">${data.strInstructions.slice(0,50)}</p>
+                        <div class="card-actions justify-start">
+                            <label onclick="loadDetail(${data.idDrink})" for="my-modal" class="btn bg-white text-orange-500 border-0 modal-button">View Details</label> 
+                        </div>
+                    </div>
+                `
+            }
+            cardContainer.appendChild(card);
+            sprint(false);
+        })
+
+    }
+    
+}
+
+document.getElementById('serach-filed').addEventListener('keypress', function (event){
+    if(event.key === "Enter"){
+        productSearchsd(12);
     }
 });
 
@@ -97,6 +112,19 @@ const displayDetail = data =>{
         </div>
     `
     modalContainer.appendChild(moderCard);
+};
+
+const productSearchsd = dataLimit => {
+    sprint(true);
+    const serachField = document.getElementById('serach-filed');
+    const value = serachField.value
+    const commonvalue = value.toLowerCase();
+    loadCocktail(commonvalue, dataLimit);
+    console.log(dataLimit);
 }
 
-loadCocktail('margarita');
+document.getElementById('view-all').addEventListener('click',function(){
+    productSearchsd();
+})
+
+loadCocktail('',10);
